e45666af89f40307d9b128e333328034cf4703cf
Naveen <somalapurinaveen@gmail.com>
2023-10-03 13:45:25 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from Cat_Or_Dog_Classification import logger
+from Cat_Or_Dog_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to my custom log")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>stage {STAGE_NAME} complete <<<<<\n\nx=====")
+except Exception as e:
+    logger.exception(e)
+    raise e

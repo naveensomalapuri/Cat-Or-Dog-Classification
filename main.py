8526e3394cbb095ad9810e89f11edeed7fcf55f7
Naveen <somalapurinaveen@gmail.com>
2023-10-04 13:56:26 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Cat_Or_Dog_Classification import logger
 from Cat_Or_Dog_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Cat_Or_Dog_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -9,6 +10,21 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>stage {STAGE_NAME} complete <<<<<\n\nx=====")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"***************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===========x")
 except Exception as e:
     logger.exception(e)
     raise e
